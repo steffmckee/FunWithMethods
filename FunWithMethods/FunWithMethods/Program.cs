1cfb4cd0d9620e54c3aae278de12b18e34ea305c
Steff McKee <steffmckee@outlook.com>
2017-05-04 14:01:50 -0600

    Add method added

--- FunWithMethods/FunWithMethods/Program.cs
+++ FunWithMethods/FunWithMethods/Program.cs
@@ -8,8 +8,23 @@ namespace FunWithMethods
 {
     class Program
     {
+        static int Add(int x, int y)
+        {
+            int ans = x + y;
+            x = 10000;
+            y = 88888;
+            return ans;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("**** Fun With Methods *****\n");
+
+            int x = 9, y = 10;
+            Console.WriteLine("Before call: X: {0}, Y: {1}", x, y);
+            Console.WriteLine("Answer is {0}", Add(x, y));
+            Console.WriteLine("Before call: X: {0}, Y: {1}", x, y);
+            Console.ReadLine();
         }
     }
 }
